003cf11fa0e6e736c11669c66692eb472c9e73af
K1r1Cakes <akiriachetty4@gmail.com>
2026-08-12 14:52:51 +0200

    Can move existing portals around

--- Assets/Scripts/Portal Gun.cs
+++ Assets/Scripts/Portal Gun.cs
@@ -13,7 +13,6 @@ public class PortalGun : MonoBehaviour
     {
         Debug.Log("ShootBlue function called");
         if (!context.performed) return;
-        Debug.Log("BLUE FIRE");
         ShootPortal(bluePortal);
     }
 
@@ -21,7 +20,6 @@ public class PortalGun : MonoBehaviour
     {
         Debug.Log("ShootOrange function called");
         if (!context.performed) return;
-        Debug.Log("ORANGE FIRE");
         ShootPortal(orangePortal);
     }
 
@@ -32,15 +30,15 @@ public class PortalGun : MonoBehaviour
         Debug.Log("ShootPortal called with: " + portal.name);
 
         Vector2 mousePosition = camera.ScreenToWorldPoint(Mouse.current.position.ReadValue()); //Get mouse position
-        Vector2 direction = mousePosition - (Vector2)firePoint.position; //Get direction of mouse
+        Vector2 direction = mousePosition - (Vector2)camera.transform.position; //Get direction of mouse
 
         direction.Normalize();
 
-        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, direction, 100f, portalLayer); //Detect the portal layer
-
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, direction, 100f, portalLayer); //Detect the portal layer
+ 
         if (hit.collider != null)
         {
-             Debug.Log("Hit: " + hit.collider.name);
+            Debug.Log("Hit: " + hit.collider.name);
             Debug.Log("Moving portal: " + portal.name);
 
             // portal.transform.position = hit.point + hit.normal * 0.05f;
@@ -48,9 +46,8 @@ public class PortalGun : MonoBehaviour
             // float angle = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg; //Calculate the angle
 
             // portal.transform.rotation = Quaternion.Euler(0,0,angle-90);
-             Vector2 newPosition =
-            hit.point + hit.normal * 0.1f;
-
+        Vector2 newPosition = hit.point + hit.normal * 0.1f;
+ 
         Debug.Log("New portal position: " + newPosition);
 
         portal.transform.position = newPosition;
@@ -59,6 +56,8 @@ public class PortalGun : MonoBehaviour
             "Actual portal position: " +
             portal.transform.position
         );
+
+  
         }
         else
 {
